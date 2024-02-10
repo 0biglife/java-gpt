5dafbb662e3d8c2d8f60bdcdea1795ed9d649c73
WondermoveDeadpool <deadpool@wondermove.net>
2024-02-10 17:15:39 +0900

    first commit

---
+++ src/test/java/com/example/atobackend/AtoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
